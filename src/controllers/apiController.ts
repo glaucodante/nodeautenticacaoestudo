9f136d124e6373ce4f0579ecdd74d1535cfb8611
glaucodante <glaucohdev@gmail.com>
2024-09-22 06:54:12 -0300

    finish aula

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -1,20 +1,29 @@
 import { Request, Response } from 'express';
+import JWT from 'jsonwebtoken'
+import dotenv from 'dotenv' // para acessar o .env
 import { User } from '../models/User';
 
+dotenv.config() // Acessando o .ENV
+
 export const ping = (req: Request, res: Response) => {
-    res.json({pong: true});
+    res.json({ pong: true });
 }
 
 export const register = async (req: Request, res: Response) => {
-    if(req.body.email && req.body.password) {
+    if (req.body.email && req.body.password) {
         let { email, password } = req.body;
 
-        let hasUser = await User.findOne({where: { email }});
-        if(!hasUser) {
+        let hasUser = await User.findOne({ where: { email } });
+        if (!hasUser) {
             let newUser = await User.create({ email, password });
 
+            const token = JWT.sign( // GERANDO O TOKEN - PEGANDO OS DADOS
+                { id: newUser.id, email: newUser.email },  // 1° param. OBJ. P/ ARMAZENAR
+                process.env.JWT_SECRET_KEY as string, // 2° param. CHAVE PRIVADA (COLOCADA NO .ENV = JWT_SECRET_KEY)
+                { expiresIn: '2h' } // 3° param. p/ COLOCAR OUTRAS OPÇÕES - EXPIRAÇÃO, por exemplo
+            )
             res.status(201);
-            res.json({ id: newUser.id });
+            res.json({ id: newUser.id, token }); // Devolvendo a RESPOSTA COM O TOKEN
         } else {
             res.json({ error: 'E-mail já existe.' });
         }
@@ -24,30 +33,34 @@ export const register = async (req: Request, res: Response) => {
 }
 
 export const login = async (req: Request, res: Response) => {
-    if(req.body.email && req.body.password) {
+    if (req.body.email && req.body.password) {
         let email: string = req.body.email;
         let password: string = req.body.password;
 
-        let user = await User.findOne({ 
+        let user = await User.findOne({
             where: { email, password }
         });
-
-        if(user) {
-            res.json({ status: true });
+        // criando o TOKEN (JWT não necessita de assincronissidade)
+        if (user) {
+            const token = JWT.sign( // GERANDO O TOKEN - PEGANDO OS DADOS
+                { id: user.id, email: user.email },  // 1° param. OBJ. P/ ARMAZENAR
+                process.env.JWT_SECRET_KEY as string, // 2° param. CHAVE PRIVADA (COLOCADA NO .ENV = JWT_SECRET_KEY)
+                { expiresIn: '2h' } // 3° param. p/ COLOCAR OUTRAS OPÇÕES - EXPIRAÇÃO, por exemplo
+            )
+            res.json({ status: true, token }); // Devolvendo a RESPOSTA COM O TOKEN
             return;
         }
     }
 
     res.json({ status: false });
 }
-
+// LISTANDO USUÁRIOS
 export const list = async (req: Request, res: Response) => {
     let users = await User.findAll();
     let list: string[] = [];
 
-    for(let i in users) {
-        list.push( users[i].email );
+    for (let i in users) {
+        list.push(users[i].email);
     }
-
     res.json({ list });
 }
--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,10 +1,38 @@
-import { error } from 'console'
 import { Request, Response, NextFunction } from 'express'
-//responsável pelo processo de autenticação
+import JWT from 'jsonwebtoken'
+import dotenv from 'dotenv'
+
+
+dotenv.config()  // para acessar .ENV
+
+// RESPONSÁVEL PELO PROCESSO DE AUTENTICAÇÃO - JWT
+// Usar a biblioteca jsonwebtoken para gerar o token
 export const Auth = {
-    private: (req: Request, res: Response, next: NextFunction) => {
-        // fazer verificação de auth
+    private: async (req: Request, res: Response, next: NextFunction) => {
+        // fazer verificação de auth com TOKEN
         let success = false
+        // console.log(req.headers.authorization)
+
+        if (req.headers.authorization) {
+            // Bearer = portador
+            // Bearer = tipo de token que é utilizado como um método de autenticação
+            const [authType, token] = req.headers.authorization.split(' ')
+            if (authType === 'Bearer') {
+                try {
+                    // console.log("TOKEN", token)
+                    // DECODIFICANDO O JWT (biblioteca)                    
+                    const decoded = JWT.verify(token, process.env.JWT_SECRET_KEY as string)
+                    success = true
+
+                    // console.log("DECODED", decoded)
+
+                } catch (err) {
+                    // console.log("Erro:", "Deu erro no JWT")
+                }
+            }
+        }
+
+
         if (success) {
             next()
         } else {
@@ -13,3 +41,6 @@ export const Auth = {
         }
     }
 }
+//PAREI NO VIDEO
+// FAZENDO AUTENTICAÇÃO BASIC AUTH
+
